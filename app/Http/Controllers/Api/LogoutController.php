f8be054ef6d87fbe149eae751c323809dfc6ee6e
tektungg <2241720128@student.polinema.ac.id>
2024-05-07 19:57:33 +0700

    JS10 Praktikum 3

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request) {
+        // remove token
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            // return resoponse JSON
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil!',
+            ]);
+        }
+    }
+}
